6f4cfe3a5f0e0780e3075d169e6b961871fd64a9
Michaela07313 <miminka07313@gmail.com>
2017-07-18 23:00:58 +0300

    add commands - append, prepend, reverse and end

--- listProcessing/listProcessor.js
+++ listProcessing/listProcessor.js
@@ -3,13 +3,29 @@ function listProcessor(input) {
     console.log(string);
 
     for (let command of input) {
-        let tokens = command.split("\s+");
+        let tokens = command.split(/\s+/g);
+        let commandProperties = tokens.slice(1);
 
-        processCommand(tokens[0], tokens);
+        processCommand(tokens[0], commandProperties);
     }
 
     function processCommand(command, tokens) {
         switch (command) {
+            case 'append': 
+                string = string + ' ' + tokens;
+                console.log(string);
+                break;
+            case 'prepend':
+                string = tokens + ' ' + string;
+                console.log(string);
+                break;
+            case 'reverse':
+                string = string.split(' ').reverse().join(' ');
+                console.log(string);
+                break;
+            case 'end':
+                console.log('Finished');
+                break;
             //Here we make cases for every command
         }
     }
